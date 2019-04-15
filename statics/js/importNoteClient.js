2223d4fd5709942d24d99bc6878787d82271be1b
Jasnini <1041259804@qq.com>
2019-04-15 13:31:56 +0800

    use Vue.js

--- statics/js/importNoteClient.js
+++ statics/js/importNoteClient.js
@@ -1,20 +1,39 @@
+    let outer=new Vue({
+        el: '#outer',
+        data:{
+            list1:[],
+            article: '',
+            dirs: [{name:'js',dir:'js笔记'},{name:'css',dir:'css笔记'},
+                  {name:'html',dir:'html笔记'},{name:'Vue',dir:'Vue笔记'},
+                  {name:'reading',dir:'阅读小记'},{name:'about',dir:'关于'}]
+        },
+        methods:{
+            changeList(listType){
+                if (listType==='js'){
+                    this.list1=['JS','JS原型链','this的用法','事件','块级作用域','Ajax'];//文件名
+                }else if(listType==='css'){
+                    this.list1=['CSS笔记'];
+                }else if(listType==='html'){
+                    this.list1=['DOM笔记'];
+                }else if(listType==='Vue'){
+                    this.list1=[''];
+                }else if(listType==='reading'){
+                    this.list1=[''];
+                }else if(listType==='about'){
+                    this.list1=[''];
+                }
+            },
+            loadData(item) {
+                const xhr = new XMLHttpRequest();       
+                xhr.onload = () => {
+                    const text1 = xhr.responseText;
+                    this.article = text1;
+                };
+                xhr.open("get","/front-note/"+encodeURIComponent(item), true);
+                xhr.send(null);
+            }
+        },
 
+    })
 
-    let obj= document.getElementsByClassName('list2');
-    //console.log(obj);
-    for (i in Object.keys(obj)){
-        obj[i].onclick=function(){
-            let fileName=this.firstChild.nodeValue;
-            console.log(fileName);
-
-            let xhr= new XMLHttpRequest();
-            
-            xhr.onload= function (){
-                let text1=xhr.responseText;
-                console.log(text1);
-                document.getElementById('article').innerHTML=text1;
-            };
-            xhr.open("get","/front-note/"+encodeURIComponent(fileName), true);
-            xhr.send(null);
-        }
-    }
+    
