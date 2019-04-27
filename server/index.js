f2f437bf140d7f15010fbfb7f7adfb300e1a042d
Jasnini <1041259804@qq.com>
2019-04-28 00:16:18 +0800

    modify the format of notes

--- server/index.js
+++ server/index.js
@@ -1,50 +1,91 @@
-//1. 导入express
-var express = require('express');
-var path = require('path');
+// 1. 导入express
+let express = require('express');
+let path = require('path');
 
-//2. 创建express服务器
-var server = express()
+// 2. 创建express服务器
+let server = express();
 
-server.use(express.static('static')); //静态文件夹
-server.use(express.static('dist')); //静态文件夹
+server.use(express.static('static')); // 静态文件夹
+server.use(express.static('dist')); // 静态文件夹
 
-
-server.get('/front-note/*', 
-    (request,response) => {
+server.get(
+    '/front-note/*',
+    (request, response) => {
         (function() {
-            //url = require("url");
-            let fs=require('fs');
-            let showdown=require('showdown');
-            let showdownHighlight = require('showdown-highlight'); 
+            // url = require("url");
+            let fs = require('fs');
+            let showdown = require('showdown');
+            let showdownHighlight = require('showdown-highlight');
+
             // var pathname = decodeURIComponent(url.parse(request.url).pathname);
-            var pathname=decodeURIComponent(request.path);
+            let pathname = decodeURIComponent(request.path);
             console.log(pathname);
 
-            fs.readFile('.'+pathname+'.md','utf8',function(err,data){     
-                    let converter= new showdown.Converter({
-                        'tasklists': false,
-                        'ghCodeBlocks': true,
-                        extensions: [showdownHighlight],
-                        
-                    });
-                    let html=converter.makeHtml(data);
-
-                    // let markdown= require('markdown').markdown;
-                    // let html= markdown.toHTML(data);
-
-                    response.set('Content-Type','text/xml');
-                    response.send(html);
-                    // console.log(html);
-                    // console.log(str);
-                    // fn(html);
+            /* eslint-disable */
+            showdown.extension('codehighlight', function() {
+              function htmlunencode(text) {
+                return (
+                  text
+                    .replace(/&amp;/g, '&')
+                    .replace(/&lt;/g, '<')
+                    .replace(/&gt;/g, '>')
+                  );
+              }
+              return [
+                {
+                  type: 'output',
+                  filter: function (text, converter, options) {
+                    // use new shodown's regexp engine to conditionally parse codeblocks
+                    var left  = '<pre><code\\b[^>]*>',
+                        right = '</code></pre>',
+                        flags = 'g',
+                        replacement = function (wholeMatch, match, left, right) {
+                          // unescape match to prevent double escaping
+                          match = htmlunencode(match);
+                          return left + hljs.highlightAuto(match).value + right;
+                        };
+                    return showdown.helper.replaceRecursiveRegExp(text, replacement, left, right, flags);
+                  }
                 }
-            );
-    }());
-});
+              ];
+            });
+            // showdown.setFlavor('github');
+          
+          // /* eslint-enable */
 
-server.get('/', function(req,res){
+            fs.readFile('.' + pathname + '.md', 'utf8', function(err, data) {
+
+                if (err) {
+                    console.error(err);
+                    // response.status(500).send('服务器异常');
+                }
+
+                // let converter = new showdown.Converter();
+                let converter = new showdown.Converter({
+                    'tasklists': false,
+                    'ghCodeBlocks': true,
+                    tables: true,
+                    extensions: [codehighlight],
+
+                });
+                let html = converter.makeHtml(data);
+
+                // let markdown= require('markdown').markdown;
+                // let html= markdown.toHTML(data);
+
+                response.set('Content-Type', 'text/xml');
+                response.send(html);
+                // console.log(html);
+                // console.log(str);
+                // fn(html);
+            });
+        })();
+    }
+);
+
+server.get('/', function(req, res) {
     res.sendFile(path.resolve('/dist/index.html'));
-})
+});
 
 // server.get('/styles/indexStyle.css',function(req,res){
 //     res.sendFile("styles/indexStyle.css",{root: __dirname});
@@ -58,6 +99,6 @@ server.get('/', function(req,res){
 //     res.sendFile("images/icon.jpg",{root: __dirname});
 // })
 
-//4. 绑定端口
-server.listen(4040)
-console.log('启动4040')
+// 4. 绑定端口
+server.listen(4040);
+console.log('启动4040');
