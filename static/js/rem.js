fe13257a7358cdc94aa486f6c7a2a10018e9c37b
zxlg <2271721552@qq.com>
2019-05-19 23:31:15 +0800

    px to rem

---
+++ static/js/rem.js
@@ -0,0 +1,16 @@
+(function useRem() {
+    var docEl = document.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            // 基准大小
+            var baseSize = 18;
+            var clientWidth = docEl.clientWidth;
+            if (clientWidth === undefined) return;
+            // 当前页面宽度相对于375宽的缩放比例，可根据自己需要修改。
+            // 设置页面根节点字体大小
+            docEl.style.fontSize = baseSize * (clientWidth / 375) + 'px';
+        };
+    if (document.addEventListener === undefined) return;
+    window.addEventListener(resizeEvt, recalc, false);
+    recalc();
+})();
